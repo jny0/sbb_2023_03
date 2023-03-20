f5a537122575b002d08c72f765fd491625d833bc
jny0 <pjy100402@naver.com>
2023-03-20 16:34:08 +0900

    root url 리다이렉트

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list"; // "question/list" URL로 페이지를 리다이렉트
+    }
+
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
