be0b1760b5300df8b19252f78b0177c321bfab8c
jny0 <pjy100402@naver.com>
2023-03-21 13:57:00 +0900

    각 테스트 케이스의 독립성이 확보되도록 리팩토링

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,16 +1,29 @@
 package com.mysite.sbb.question;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     //리포지터리의 대상이 되는 엔티티의 타입(Question)과 해당 엔티티의 PK의 속성 타입(Integer)을 지정
+
+    @Transactional
+    // @Modifying // 만약 아래 쿼리가 SELECT가 아니라면 이걸 붙여야 한다.
+    @Modifying
+    // nativeQuery = true 여야 MySQL 쿼리문법 사용 가능
+    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
+
+
     Question findBySubject(String subject);
     // DI에 의해 스프링이 자동으로 QuestionRepository 객체를 생성한다. 이 때 프록시 패턴이 사용된다고 한다.
     // 즉 findBy + 엔티티의 속성명(예:findBySubject)과 같은 리포지터리 메서드를 작성하면 해당 속성의 값으로 데이터를 조회할수 있다.
 
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+
 }
